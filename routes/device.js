585583006bb4a60d89025316040f8c6629c42b94
pkg4446 <pkg4446@gmail.com>
2024-07-25 09:57:23 +0900

    240725

--- routes/device.js
+++ routes/device.js
@@ -9,20 +9,19 @@ router.post('/log', async function(req, res) {
 
     const path_device   = "./data/device/"+req.body.DEVID;
     const date_now      = new Date();
+    const path_log      = path_log+date_now.getFullYear()+"/"+date_now.getMonth();
+
     let file_content    = date_now+","+req.body.t_w+","+req.body.t_l+","+req.body.t_a+","+req.body.t_o+"\r\n";
     let filename        = "";
-    
-    filename += date_now.getFullYear();
-    if(date_now.getMonth()<10) filename += "0";
-    filename += date_now.getMonth();
+    if(date_now.getDate()<10) filename += "0";
     filename += date_now.getDate();
    
-    if(!file_system.check(path_device+"/log")) file_system.folderMK(path_device+"/log");
+    if(!file_system.check(path_log)) file_system.folderMK(path_log);
     file_system.fileMK(path_device,IP+"\r\n","ip");
-    if(file_system.check(path_device+"/log/"+filename+".csv")){
-        file_system.fileADD(path_device+"/log/",file_content,filename);
+    if(file_system.check(path_log+"/"+filename+".csv")){
+        file_system.fileADD(path_log,file_content,filename);
     }else{
-        file_system.fileMK(path_device+"/log/",file_content,filename);
+        file_system.fileMK(path_log,file_content,filename);
     }
     res.status(201).send("ack");
 });
