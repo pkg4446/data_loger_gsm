951d05829c64d8cbed615e7526fb3945758f975e
pkg4446 <pkg4446@gmail.com>
2024-07-25 16:30:40 +0900

    240725

--- html_viewer.js
+++ html_viewer.js
@@ -4,11 +4,12 @@ module.exports = {
     
     page:   function(FILE){ 
         let response = fs.readFileSync(`./html/header.html`, 'utf8');
-        response += fs.readFileSync(`./html/nav.html`, 'utf8');  
+        response += fs.readFileSync(`./html/css.css`, 'utf8');
+        response += fs.readFileSync(`./html/nav.html`, 'utf8');
         try {
-            response += fs.readFileSync(`./html/body/${FILE}.html`, 'utf8'); 
+            response += fs.readFileSync(`./html/body/${FILE}.html`, 'utf8');
         } catch (error) {}
-        response += fs.readFileSync(`./html/footer.html`, 'utf8');  
+        response += fs.readFileSync(`./html/footer.html`, 'utf8');
         return response;
     },
 }
