f019bd201d897a5e8ba7de4a205047c59153b325
pkg4446 <pkg4446@gmail.com>
2024-08-07 09:54:30 +0900

    240807

---
+++ mqtt.js
@@ -0,0 +1,37 @@
+const mqtt      = require("mqtt");
+
+const options   = {
+    host:       smarthive.kr,
+    port:       1883,
+    protocol:   "mqtt",
+    username:   "test",
+    password:   "test",
+};
+const client = mqtt.connect(options);
+
+client.subscribe("GSM");
+
+client.on("error", (error) => {console.log("Can't connect" + error);});
+client.on("connect", () => {console.log("connected: "+ client.connected);});
+
+client.on("message", async(topic, message) => {	
+    console.log(`토픽:${topic.toString()}, 메세지:${message.toString()}, ID:${client.getLastMessageId()}`);
+});
+//client.end();
+
+function sendMQTT(target,contents){
+    //console.log("send: ",target, "message: ",contents);
+    client.publish(target,contents,{qos:2});  
+}
+
+module.exports = {
+    send : async function(data){
+        let response = true;
+        try {
+            sendMQTT(data.TARGET, data.COMMEND);
+        } catch (error) {
+            response = false;
+        }        
+        return response;
+    },
+}
